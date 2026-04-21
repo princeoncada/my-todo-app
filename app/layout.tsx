e372453c0258591964672e2b382724da839382d1
princeoncada <prince.oncada@gmail.com>
2026-04-21 16:53:07 +0800

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
+import { TRPCReactProvider } from "@/trpc/client";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,18 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col items-center justify-center">
+        <TRPCReactProvider>
+          {children}
+          <Toaster
+            toastOptions={{
+              classNames: {
+                description: "!text-zinc-700"
+              }
+            }}
+          />
+        </TRPCReactProvider>
+      </body>
     </html>
   );
 }

8166de3a47ba6ea0fd0d5c38fe5182350335677f
princeoncada <prince.oncada@gmail.com>
2026-04-25 18:59:29 +0800

    refactor(layout): enhance MaxWidthWrapper with conditional centering

--- components/MaxWidthWrapper.tsx
+++ components/MaxWidthWrapper.tsx
@@ -1,10 +1,14 @@
 import { ReactNode } from "react";
+import { cn } from "@/lib/utils";
 
-const MaxWidthWrapper = ({ children }: { children: ReactNode; }) => {
+const MaxWidthWrapper = ({ children, singleItemPage = false }: { children: ReactNode; singleItemPage?: boolean; }) => {
   return (
-    <div className="w-full max-w-7xl flex items-center justify-center">
+    <div className={cn("w-full max-w-7xl min-h-full xl:px-0 px-2", {
+      "flex items-center justify-center": singleItemPage
+    })}>
       {children}
     </div>
+
   );
 };
 
